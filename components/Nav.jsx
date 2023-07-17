1f329222b09bf177a44e2b39ae13782148002b97
Srinjoy Das <srinjoy98x@gmail.com>
2023-07-17 15:42:52 +0530

    Fixed Profile page

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -34,7 +34,7 @@ const Nav = () => {
                <button type="button" onClick={signOut} className="outline_btn">
                  Sign Out
                </button>
-               <Link href="/profile">
+               <Link href={`/profile?id=${session?.user.id}`}>
                 <Image src={session?.user.image} width={37} height={37} className="rounded-full" alt="Profile">
 
                 </Image>
